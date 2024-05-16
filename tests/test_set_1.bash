06850f4dc32267906a37bad187bb85970d8f1837
Marc Mengel <mengel@fnal.gov>
2024-05-16 14:52:36 -0500

    almost there

--- tests/test_set_1.bash
+++ tests/test_set_1.bash
@@ -11,8 +11,8 @@ esac
 #
 prefix=$(dirname $(dirname $this))
 basedir=/tmp/fst_test_dir_$$
-spack_rel1=v0_20_0-fermi
-spack_rel2=v0_22_0-fermi
+spack_rel1=v0.21.0-fermi
+spack_rel2=v0.22.0-fermi
 
 # put us on the front of the PATH
 PATH=$prefix/bin:$PATH
@@ -20,7 +20,6 @@ PATH=$prefix/bin:$PATH
 make_basedir() {
     mkdir $basedir
     cd $basedir
-    set -x
 }
 
 cleanup_basedir() {
@@ -28,26 +27,60 @@ cleanup_basedir() {
     rm -rf $basedir
 }
 
-test_make_spack() {
+
+make_spack_args() {
     inst=upstream
 
     cd $basedir
     rm -rf $inst 
     echo "test_make_spack" >&3
     which make_spack >&3
-    make_spack -u --spack_release=$spack_rel1 $PWD/$inst 
+    make_spack "$@" $PWD/$inst 
 
-    source $inst/setup-env.sh
-    echo $SPACK_ROOT | grep $PWD/$inst 
+    echo make_spack returns $?
+
+    source $inst/setup-env.sh           
+    echo source returns $?
+    echo $SPACK_ROOT | grep $PWD/$inst  
+    echo SPACK_ROOT grep  returns $?
     spack find | grep fermi-spack-tools
+    echo spack find  grep  returns $?
+    spack find
     spack compiler list | grep gcc
+    echo spack compiler list   grep  returns $?
+    false
+}
+
+test_make_spack_u_1() {
+ make_spack_args -u --spack_release=$spack_rel1 
+}
+test_make_spack_p_1() {
+ make_spack_args -p --spack_release=$spack_rel1 
+}
+test_make_spack_u_2() {
+ make_spack_args -u --spack_release=$spack_rel2 
+}
+test_make_spack_p_2() {
+ make_spack_args -p --spack_release=$spack_rel2 
+}
+test_make_spack_u_1_p() {
+ make_spack_args -u --spack_release=$spack_rel1 --with_padding 
+}
+test_make_spack_p_1_p() {
+ make_spack_args -p --spack_release=$spack_rel1 --with_padding 
+}
+test_make_spack_u_2_p() {
+ make_spack_args -u --spack_release=$spack_rel2 --with_padding 
+}
+test_make_spack_p_2_p() {
+ make_spack_args -p --spack_release=$spack_rel2 --with_padding 
 }
 
 test_make_packages_yaml() {
     cd $basedir
     rm -rf just_packages
     make_packages_yaml $PWD/just_packages
-    ls -lR just_packages
+    grep 'spec: "python' just_packages/etc/spack/linux/*/packages.yaml
 }
 
 test_make_subspack () {
@@ -57,9 +90,9 @@ test_make_subspack () {
     rm -rf $inst
     make_subspack $PWD/upstream $PWD/$inst
 
-    source $inst/setup-env.sh
-    echo $SPACK_ROOT | grep $PWD/$inst 
-    spack find | grep fermi-spack-tools
+    source $inst/setup-env.sh           &&
+    echo $SPACK_ROOT | grep $PWD/$inst  && 
+    spack find | grep fermi-spack-tools &&
     spack compiler list | grep gcc
 }
 
@@ -70,9 +103,9 @@ test_bootstrap () {
 
     bootstrap $PWD/$inst
 
-    source $inst/setup-env.sh
-    echo $SPACK_ROOT | grep $PWD/$inst 
-    spack find | grep fermi-spack-tools
+    source $inst/setup-env.sh           &&
+    echo $SPACK_ROOT | grep $PWD/$inst  &&
+    spack find | grep fermi-spack-tools &&
     spack compiler list | grep gcc
 }
 test_declare_simple () {
@@ -123,9 +156,17 @@ source ./unittest.bash
 testsuite fermi_spack_tools_tests \
     -s make_basedir \
     -t cleanup_basedir \
-    test_make_spack  \
-    test_make_packages_yaml  \
-    test_make_subspack  \
-    test_bootstrap  \
+    test_make_spack_u_1 \
+
+#    test_make_spack_p_1 \
+#    test_make_spack_u_2 \
+#    test_make_spack_p_2 \
+#    test_make_spack_u_1_p \
+#    test_make_spack_p_1_p \
+#    test_make_spack_u_2_p \
+#    test_make_spack_p_2_p \
+#    test_make_packages_yaml  \
+#    test_make_subspack  \
+#    test_bootstrap  \
 
 fermi_spack_tools_tests "$@"
