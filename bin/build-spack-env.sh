13cc8357a95fbd2f6a516d4f38ac3afe4d1954bf
Patrick Gartung <gartung@fnal.gov>
2026-09-15 11:00:03 -0500

    Always update buildcache index

--- bin/build-spack-env.sh
+++ bin/build-spack-env.sh
@@ -900,15 +900,12 @@ EOF
            "${hashes_to_cache[@]/#//}" || \
         _die "failure caching packages to $cache" 
     done
-    if [ -d "$cache/blobs" ] &&
-         [ -f "$cache/v3/layout.json*" ]; then
         _report $PROGRESS "updating build cache index at $cache"
         _cmd $DEBUG_1 $PROGRESS \
         spack \
         ${common_spack_opts[*]:+"${common_spack_opts[@]}"} \
         buildcache update-index -k "$cache" ||
         _report $ERROR "failure to update build cache index: manual intervention required for $cache"
-    fi
   fi
 }
 
